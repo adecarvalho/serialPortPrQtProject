70439da8829058cf7d38263be37160a8b7d22e41
adecarvalho <decarvalho.adelino@gmail.com>
2023-02-23 07:44:07 +0000

    place led external

--- main.cpp
+++ main.cpp
@@ -4,26 +4,73 @@
 void setup();
 void loop();
 void flash();
+void serialHandler();
+void sendMessage();
 
 // obj
 DigitalOut ledInternal(D13);
+DigitalOut ledExternal(D9);
 RawSerial pc(SERIAL_TX, SERIAL_RX);
+
+//***************************
+void serialHandler() {
+  //
+  if (pc.readable()) {
+    char val = pc.getc();
+
+    switch(val)
+    {
+        case 'a':
+        ledExternal=1;
+        break;
+        //
+        case 'z':
+        ledExternal=0;
+        break;
+        //
+        default:
+        break;
+    }
+
+    //pc.putc(val);
+  }
+}
+
+//***************************
+void sendMessage() {
+  static int cmp = 100;
+  //
+  pc.printf("D%dT\n\r", cmp);
+  //
+  cmp += 1;
+  if (cmp > 1000) {
+    cmp = 100;
+  }
+}
 
 //***************************
 void flash() {
   for (int i = 0; i < 3; i++) {
     ledInternal = 1;
     wait_us(100000);
+    //
     ledInternal = 0;
     wait_us(100000);
   }
 }
 
 //****************************
-void setup() { ledInternal = 0; }
+void setup() {
+  //
+  ledInternal = 0;
+  pc.baud(115200);
+  pc.attach(&serialHandler, Serial::RxIrq);
+}
 //****************************
 void loop() {
   flash();
+  //
+  sendMessage();
   //
   thread_sleep_for(2000);
 }
