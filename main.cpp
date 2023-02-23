4c04c113f114b79caacf221027464dd9e8431139
adecarvalho <decarvalho.adelino@gmail.com>
2023-02-23 07:10:49 +0000

    flash prog

--- main.cpp
+++ main.cpp
@@ -3,18 +3,29 @@
 // prototypes
 void setup();
 void loop();
+void flash();
 
 // obj
 DigitalOut ledInternal(D13);
+RawSerial pc(SERIAL_TX, SERIAL_RX);
+
+//***************************
+void flash() {
+  for (int i = 0; i < 3; i++) {
+    ledInternal = 1;
+    wait_us(100000);
+    ledInternal = 0;
+    wait_us(100000);
+  }
+}
 
 //****************************
 void setup() { ledInternal = 0; }
 //****************************
 void loop() {
-  ledInternal = 1;
-  thread_sleep_for(100);
-  ledInternal = 0;
-  thread_sleep_for(800);
+  flash();
+  //
+  thread_sleep_for(2000);
 }
 
 //****************************
